00fd95ffe40de034d4db0f3318cadd5d7608da9d
Digneli Davila <digneli14@gmail.com>
2020-11-26 08:54:34 -0300

    cambios, agregando DB falta hacer la conexion

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 'use strict'
 
+require('dotenv').config()
 const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+const { makeExecutableSchema } = require('graphql-tools')
 
 const app = express()
 const port = process.env.port || 3000
 
 // definiendo el esquema
-const schema = buildSchema(
-  readFileSync(
+const typeDefs = readFileSync(
     join(__dirname, 'lib', 'schema.graphql'),
     'utf-8'
-  )
 )
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
